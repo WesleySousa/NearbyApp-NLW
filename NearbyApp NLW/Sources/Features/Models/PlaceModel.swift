8211c1e4fa5845c207bc69b858d3152aca67b2cb
Wesley Silva <wesley.sousa59@gmail.com>
2024-12-18 13:41:52 -0300

    feat: creating PlaceTableView

---
+++ NearbyApp NLW/Sources/Features/Models/PlaceModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlaceModel.swift
+//  NearbyApp NLW
+//
+//  Created by Wesley Silva on 16/12/24.
+//
+
+import Foundation
+
+struct Place {
+    let imageName: String
+    let title: String
+    let description: String
+}
--- NearbyApp NLW/Sources/Features/Scenes/Home/TableCell/PlaceTableViewCell.swift
+++ NearbyApp NLW/Sources/Features/Scenes/Home/TableCell/PlaceTableViewCell.swift
@@ -55,7 +55,16 @@ class PlaceTableViewCell: UITableViewCell {
         return label
     }()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 8
+        view.layer.borderWidth = 1
+        view.layer.borderColor = Colors.gray200.cgColor
+        return view
+    }()
+    
+    override convenience init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         self.init(style: style, reuseIdentifier: reuseIdentifier)
         setUpUI()
     }
@@ -65,10 +74,50 @@ class PlaceTableViewCell: UITableViewCell {
     }
     
     private func setUpUI() {
+        addSubview(containerView)
+        containerView.addSubview(itemImageView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(descriptionLabel)
+        containerView.addSubview(ticketIcon)
+        containerView.addSubview(ticketLabel)
+        
         setUpConstraints()
     }
     
     private func setUpConstraints() {
-        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            containerView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            
+            itemImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
+            itemImageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            itemImageView.widthAnchor.constraint(equalToConstant: 116),
+            itemImageView.heightAnchor.constraint(equalToConstant: 104),
+            
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: itemImageView.trailingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            descriptionLabel.leadingAnchor.constraint(equalTo: itemImageView.trailingAnchor, constant: 8),
+            descriptionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8),
+            
+            ticketIcon.leadingAnchor.constraint(equalTo: itemImageView.trailingAnchor, constant: 8),
+            ticketIcon.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8),
+            ticketIcon.widthAnchor.constraint(equalToConstant: 13),
+            ticketIcon.heightAnchor.constraint(equalToConstant: 11),
+             
+            ticketLabel.centerYAnchor.constraint(equalTo: ticketIcon.centerYAnchor),
+            ticketLabel.leadingAnchor.constraint(equalTo: ticketIcon.trailingAnchor, constant: 4),
+        ])
+    }
+    
+    func configure(with place: Place) {
+        itemImageView.image = UIImage(named: place.imageName)
+        titleLabel.text = place.title
+        descriptionLabel.text = place.description
+        ticketLabel.text = "cupons disponíveis"
     }
 }
